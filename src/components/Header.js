9777629fa679a5aa7909a2bbba2aa1ce396dfe57
Mike <michael.barbone@gmail.com>
2019-09-02 17:42:56 -0400

    Header image, edit button links to edit page and passes Id into url params.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import GoogleAuth from "./GoogleAuth";
+import logo from "../logo.png"; // Tell Webpack this JS file uses this image
 
 const Header = () => {
+  const style = { width: "100px" };
+
   return (
     <div className="ui secondary pointing menu">
       <Link to="/" className="item">
-        Streamy
+        <img src={logo} alt="Streamy Logo" style={style} />
       </Link>
       <div className="right menu">
         <Link to="/" className="item">
           All Streams
         </Link>
-        <GoogleAuth />
+
+        <div className="item">
+          <GoogleAuth />
+        </div>
       </div>
     </div>
   );
